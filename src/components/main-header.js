3da28c4ed5970f84d6870bb05b2f1dabd90eab8f
grhu <mic.grochowski@gmail.com>
2018-02-14 18:54:49 +0100

    v1.2.0
    Some RWD fixes

--- src/components/main-header.js
+++ src/components/main-header.js
@@ -5,6 +5,7 @@ const MainHeader = (props) => {
   return (
     <header className="main-header">
       <h1 className="main-header__title">Front-end Developer useful links</h1>
+      <p className="main-header__subtitle">A small collection of useful guides and tutorials for learning front-end development.</p>
     </header>
   )
 }
